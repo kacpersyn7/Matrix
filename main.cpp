4384f8ead376cca2c557263689e957980758f2df
kacpersyn <kacper@student.agh.edu.pl>
2016-05-10 10:56:59 +0200

    0.01

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: kacper
+ *
+ * Created on 10 maja 2016, 10:53
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
