78261e44e8786637bf590dea0ac66632db7e1a0d
kacpersyn <kacper@student.agh.edu.pl>
2016-05-11 00:27:14 +0200

    0.011

---
+++ Vector.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Vector.h
+ * Author: kacper
+ *
+ * Created on 10 maja 2016, 10:59
+ */
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector {
+public:
+    Vector();
+    Vector(const Vector& orig);
+    virtual ~Vector();
+private:
+
+};
+
+#endif /* VECTOR_H */
+
